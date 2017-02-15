09065da076fef8a34280c77dfa6a6e0b5fb056bc
Arun Kumar <arunkumar@AppleMac.local>
2017-02-15 11:29:47 +0530

    Folder name modified

---
+++ FDA/FDA/ActivityResultModel/ActvityResult.swift
@@ -0,0 +1,16 @@
+//
+//  ActvityResult.swift
+//  FDA
+//
+//  Created by Arun Kumar on 2/15/17.
+//  Copyright © 2017 BTC. All rights reserved.
+//
+
+import Foundation
+
+import ResearchKit
+
+
+class ActvityResult {
+    
+}

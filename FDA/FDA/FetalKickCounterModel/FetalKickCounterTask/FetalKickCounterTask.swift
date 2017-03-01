7112ca6fb6cb011c55c209d0cf36af931c6cef73
Arun Kumar <arunkumar@AppleMac.local>
2017-03-01 11:30:43 +0530

    fetalkickcountertaskView Controller class added

--- FDA/FDA/FetalKickCounterModel/FetalKickCounterTask/FetalKickCounterTask.swift
+++ FDA/FDA/FetalKickCounterModel/FetalKickCounterTask/FetalKickCounterTask.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import ResearchKit
+
+class FetalKickCounterTask {
+    
+    
+    var steps:[ORKStep]? = [ORKStep]()
+    init() {
+        
+    }
+    
+    func getTask() -> ORKOrderedTask {
+        let instructionStep = ORKInstructionStep(identifier: "1234")
+        instructionStep.title = NSLocalizedString("KickTitle", comment: "")
+        
+        instructionStep.text = "InstructionStep"
+        
+        steps?.append(instructionStep)
+        
+        
+        let kickStep = FetalKickCounterStep(identifier: "FetalKickCounter")
+        
+        kickStep.counDownTimer = 30
+        kickStep.totalCounts = 0
+        
+        
+        
+        
+        kickStep.stepDuration = 20
+        kickStep.shouldShowDefaultTimer = false
+        kickStep.shouldStartTimerAutomatically = true
+        kickStep.shouldContinueOnFinish = true
+        kickStep.title = "Please listen for 30 seconds."
+        
+        steps?.append(kickStep)
+        
+        let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
+        steps?.append(summaryStep)
+        
+        return ORKOrderedTask(identifier: "FetalKickCounterTask", steps: steps)
+    }
+    
+    
+   
+}

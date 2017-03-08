da4cdc4266a8c54d277f3b9d45d91f1bf8e73d9c
Surender Rathore <surenders@boston-technology.com>
2017-03-08 12:39:56 +0530

    Customize navigation bar on application launch

--- FDA/FDA/AppDelegate.swift
+++ FDA/FDA/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.customizeNavigationBar()
+        
         return true
     }
 
@@ -39,6 +42,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    //MARK:Custom Navigation Bar
+    func customizeNavigationBar(){
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-Medium", size: 18)!
+        ]
+    
     }
 
 

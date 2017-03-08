97838f8faedc8b4fa9c5da2b625939e627941d24
Arun Kumar <arunkumar@AppleMac.local>
2017-03-08 18:49:46 +0530

    confirmation optional cell implemented

--- FDA/FDA/Controllers/GatewayUI/ConfirmationViewUI/ConfirmationOptionalTableViewCell.swift
+++ FDA/FDA/Controllers/GatewayUI/ConfirmationViewUI/ConfirmationOptionalTableViewCell.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
+
+let kDeleteButtonTag = 11220
+let kRetainButtonTag = 11221
+
+let kConfirmationOptionalDefaultTypeRetain = "retain"
+let kConfirmationOptionalDefaultTypeDelete = "delete"
+
 class ConfirmationOptionalTableViewCell: UITableViewCell {
 
+    @IBOutlet var buttonDeleteData:UIButton?
+    @IBOutlet var buttonRetainData:UIButton?
+    @IBOutlet var labelTitle:UILabel?
+    @IBOutlet var imageViewDeleteCheckBox: UIImageView?
+    @IBOutlet var imageViewRetainCheckBox: UIImageView?
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+//MARK:IBActions
+    
+    @IBAction func deleteOrRetainDataButtonAction(_ sender:UIButton?){
+        
+        if sender?.tag == kDeleteButtonTag {
+            if (imageViewDeleteCheckBox?.image?.isEqual(#imageLiteral(resourceName: "notChecked")))! {
+                imageViewDeleteCheckBox?.image = #imageLiteral(resourceName: "checked")
+                imageViewRetainCheckBox?.image = #imageLiteral(resourceName: "notChecked")
+            }
+            else{
+                imageViewDeleteCheckBox?.image = #imageLiteral(resourceName: "notChecked")
+                imageViewRetainCheckBox?.image = #imageLiteral(resourceName: "checked")
+            }
+        }
+        else{
+            if (imageViewRetainCheckBox?.image?.isEqual(#imageLiteral(resourceName: "notChecked")))! {
+                imageViewRetainCheckBox?.image = #imageLiteral(resourceName: "checked")
+                imageViewDeleteCheckBox?.image = #imageLiteral(resourceName: "notChecked")
+            }
+            else{
+                imageViewRetainCheckBox?.image = #imageLiteral(resourceName: "notChecked")
+                imageViewDeleteCheckBox?.image = #imageLiteral(resourceName: "checked")
+            }
+        }
+       
+    }
+   
+    //MARK:Utility methods
+    
+    func setDefaultDeleteAction(defaultValue:String){
+        
+        if defaultValue == kConfirmationOptionalDefaultTypeRetain {
+            imageViewRetainCheckBox?.image = #imageLiteral(resourceName: "notChecked")
+            imageViewDeleteCheckBox?.image = #imageLiteral(resourceName: "checked")
+
+        }
+        else{
+            imageViewDeleteCheckBox?.image = #imageLiteral(resourceName: "checked")
+            imageViewRetainCheckBox?.image = #imageLiteral(resourceName: "notChecked")
+        }
+        
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 

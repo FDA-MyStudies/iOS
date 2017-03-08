417e940b9113c8d766bd2e1de3bf4e8d55744e3b
Arun Kumar <arunkumar@AppleMac.local>
2017-03-08 18:39:14 +0530

    Confirmation controller , ConfirmationOptionalCell , ConfirmationTableViewCell created & plist added, reference image added for testing

---
+++ FDA/FDA/Controllers/GatewayUI/ConfirmationViewUI/ConfirmationOptionalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConfirmationOptionalTableViewCell.swift
+//  FDA
+//
+//  Created by Arun Kumar on 3/8/17.
+//  Copyright © 2017 BTC. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmationOptionalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FDA/FDA/Controllers/GatewayUI/ConfirmationViewUI/ConfirmationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConfirmationTableViewCell.swift
+//  FDA
+//
+//  Created by Arun Kumar on 3/8/17.
+//  Copyright © 2017 BTC. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

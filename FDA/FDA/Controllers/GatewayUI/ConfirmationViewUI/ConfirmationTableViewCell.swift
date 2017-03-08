975384ed177bd5c1d0fc4ca22f3fe02474ec57ef
Arun Kumar <arunkumar@AppleMac.local>
2017-03-08 18:49:20 +0530

    confirmation tableview cell implemented

--- FDA/FDA/Controllers/GatewayUI/ConfirmationViewUI/ConfirmationTableViewCell.swift
+++ FDA/FDA/Controllers/GatewayUI/ConfirmationViewUI/ConfirmationTableViewCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ConfirmationTableViewCell: UITableViewCell {
 
+     @IBOutlet var labelTitle:UILabel?
+     @IBOutlet var labelTitleDescription:UILabel?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code

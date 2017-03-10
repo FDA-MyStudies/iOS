1d840289af33d9ea338496dbebac53bf2d2ff29a
Surender Rathore <surenders@boston-technology.com>
2017-03-10 16:13:25 +0530

    String encoder file to base64

--- FDA/FDA/Controllers/GatewayUI/Extension/StringEncoder.swift
+++ FDA/FDA/Controllers/GatewayUI/Extension/StringEncoder.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2017 BTC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self) else {
+            return nil
+        }
+        
+        return String(data: data, encoding: .utf8)
+    }
+    
+    func toBase64() -> String {
+        return Data(self.utf8).base64EncodedString()
+    }
+}

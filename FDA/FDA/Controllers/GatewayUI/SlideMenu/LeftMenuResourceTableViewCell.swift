49a70bde42080b6cd3ca18a5640079c0d0a725b9
Arun Kumar <arunkumar@AppleMac.local>
2017-03-10 19:22:09 +0530

    New Assets added for left menu and study

---
+++ FDA/FDA/Controllers/GatewayUI/SlideMenu/LeftMenuResourceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeftMenuResourceTableViewCell.swift
+//  FDA
+//
+//  Created by Arun Kumar on 3/10/17.
+//  Copyright © 2017 BTC. All rights reserved.
+//
+
+import UIKit
+
+class LeftMenuResourceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

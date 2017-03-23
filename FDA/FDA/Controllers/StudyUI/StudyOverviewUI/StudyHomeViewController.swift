7fbe3c490febba3bc0e6d1684fea9f3eaf6c02ba
Arun Kumar <arunkumar@AppleMac.local>
2017-03-23 15:35:13 +0530

    Crashes Fixed :StudyId was getting nulled after adding realm, added checks

--- FDA/FDA/Controllers/StudyUI/StudyOverviewUI/StudyHomeViewController.swift
+++ FDA/FDA/Controllers/StudyUI/StudyOverviewUI/StudyHomeViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import ResearchKit
 
 class StudyHomeViewController : UIViewController{
     
@@ -38,7 +39,12 @@ class StudyHomeViewController : UIViewController{
         if User.currentUser.userType == UserType.AnonymousUser {
             buttonStar.isHidden = true
         }
-        
+        else{
+            
+            if Study.currentStudy !=  nil && Study.currentStudy?.studyId != nil {
+                WCPServices().getEligibilityConsentMetadata(studyId:(Study.currentStudy?.studyId)! , delegate: self as! NMWebServiceDelegate)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -95,8 +101,11 @@ class StudyHomeViewController : UIViewController{
             let leftController = slideMenuController()?.leftViewController as! LeftMenuViewController
             leftController.changeViewController(.profile_signin)
         }
+        else{
+            self.createEligibilityConsentTask()
+        }
     }
-
+    
     
     @IBAction func backButtonAction(_ sender: Any) {
         
@@ -111,6 +120,49 @@ class StudyHomeViewController : UIViewController{
             (sender as! UIButton).isSelected = !(sender as! UIButton).isSelected
         }
     }
+    
+    func createEligibilityConsentTask()   {
+        
+        var eligibilitySteps =  EligibilityBuilder.currentEligibility?.getEligibilitySteps()
+        
+        let taskViewController:ORKTaskViewController?
+        
+        
+        
+        
+        let filePath  = Bundle.main.path(forResource: "Consent", ofType: "json")
+        let data = NSData(contentsOfFile: filePath!)
+        do {
+            let dataDict = try JSONSerialization.jsonObject(with: data! as Data, options: []) as? Dictionary<String,Any>
+            
+            let consent = dataDict?["Result"]as! Dictionary<String, Any>
+            ConsentBuilder.currentConsent = ConsentBuilder()
+            ConsentBuilder.currentConsent?.initWithMetaData(metaDataDict: consent)
+            
+            
+        }catch{
+            
+        }
+        
+        let consentTask:ORKOrderedTask? =  ConsentBuilder.currentConsent?.createConsentTask() as! ORKOrderedTask?
+        
+        for stepDict in (consentTask?.steps)!{
+            eligibilitySteps?.append(stepDict)
+        }
+        
+        
+        
+        let orkOrderedTask:ORKTask? = ORKOrderedTask(identifier: "Eligibility_ConsentTask", steps: eligibilitySteps)
+        
+        taskViewController = ORKTaskViewController(task:orkOrderedTask, taskRun: nil)
+        taskViewController?.delegate = self
+        taskViewController?.outputDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        present(taskViewController!, animated: true, completion: nil)
+        
+        
+    }
+    
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let pageViewController = segue.destination as? PageViewController {
@@ -150,4 +202,178 @@ extension StudyHomeViewController: PageViewControllerDelegate {
         }
     }
     
+}
+
+
+extension StudyHomeViewController:NMWebServiceDelegate {
+    func startedRequest(_ manager: NetworkManager, requestName: NSString) {
+        Logger.sharedInstance.info("requestname : \(requestName)")
+        //self.addProgressIndicator()
+    }
+    func finishedRequest(_ manager: NetworkManager, requestName: NSString, response: AnyObject?) {
+        Logger.sharedInstance.info("requestname : \(requestName)")
+        
+        //self.removeProgressIndicator()
+        
+    }
+    func failedRequest(_ manager: NetworkManager, requestName: NSString, error: NSError) {
+        Logger.sharedInstance.info("requestname : \(requestName)")
+        //self.removeProgressIndicator()
+        
+        
+        
+    }
+}
+
+
+extension StudyHomeViewController:ORKTaskViewControllerDelegate{
+    //MARK:ORKTaskViewController Delegate
+    
+    
+    func taskViewControllerSupportsSaveAndRestore(_ taskViewController: ORKTaskViewController) -> Bool {
+        return true
+    }
+    
+    public func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        
+        
+        var taskResult:Any?
+        
+        switch reason {
+            
+        case ORKTaskViewControllerFinishReason.completed:
+            print("completed")
+            taskResult = taskViewController.result
+        case ORKTaskViewControllerFinishReason.failed:
+            print("failed")
+            taskResult = taskViewController.result
+        case ORKTaskViewControllerFinishReason.discarded:
+            print("discarded")
+            taskResult = taskViewController.result
+        case ORKTaskViewControllerFinishReason.saved:
+            print("saved")
+            taskResult = taskViewController.restorationData
+            
+            if taskViewController.task?.identifier == "ConsentTask"{
+                
+            }
+            else{
+                activityBuilder?.activity?.restortionData = taskViewController.restorationData
+            }
+        }
+        
+        if  taskViewController.task?.identifier == "ConsentTask"{
+            consentbuilder?.consentResult?.initWithORKTaskResult(taskResult:taskViewController.result )
+        }
+        else{
+            activityBuilder?.actvityResult?.initWithORKTaskResult(taskResult: taskViewController.result)
+        }
+        
+        
+        taskViewController.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func taskViewController(_ taskViewController: ORKTaskViewController, stepViewControllerWillAppear stepViewController: ORKStepViewController) {
+        
+        if (taskViewController.result.results?.count)! > 1{
+            
+            
+            if activityBuilder?.actvityResult?.result?.count == taskViewController.result.results?.count{
+                activityBuilder?.actvityResult?.result?.removeLast()
+            }
+            else{
+                
+                if (activityBuilder?.actvityResult?.result?.count)! < (taskViewController.result.results?.count)!{
+                    
+                    let orkStepResult:ORKStepResult? = taskViewController.result.results?[(taskViewController.result.results?.count)! - 2] as! ORKStepResult?
+                    let activityStepResult:ActivityStepResult? = ActivityStepResult()
+                    
+                    activityStepResult?.initWithORKStepResult(stepResult: orkStepResult! as ORKStepResult , activityType:(activityBuilder?.actvityResult?.activity?.type)!)
+                    activityBuilder?.actvityResult?.result?.append(activityStepResult!)
+                    
+                }
+            }
+        }
+    }
+    
+    //MARK:StepViewController Delegate
+    public func stepViewController(_ stepViewController: ORKStepViewController, didFinishWith direction: ORKStepViewControllerNavigationDirection){
+        
+    }
+    
+    public func stepViewControllerResultDidChange(_ stepViewController: ORKStepViewController){
+        
+    }
+    public func stepViewControllerDidFail(_ stepViewController: ORKStepViewController, withError error: Error?){
+        
+    }
+    func taskViewController(_ taskViewController: ORKTaskViewController, viewControllerFor step: ORKStep) -> ORKStepViewController? {
+        
+        if step.identifier == "EligibilityTokenStep" {
+            
+            let gatewayStoryboard = UIStoryboard(name: "FetalKickCounter", bundle: nil)
+            
+            let ttController = gatewayStoryboard.instantiateViewController(withIdentifier: "EligibilityStepViewController") as! EligibilityStepViewController
+            ttController.step = step
+            
+            
+            return ttController
+        } else {
+            return nil
+        }
+    }
+    
+    
+    func buildTask()  {
+        
+        // let filePath  = Bundle.main.path(forResource: "LatestActive_Taskdocument", ofType: "json")
+        
+        //let filePath  = Bundle.main.path(forResource: "ActiveTask", ofType: "json")
+        
+        let filePath  = Bundle.main.path(forResource: "Consent", ofType: "json")
+        
+        //let filePath  = Bundle.main.path(forResource: "Acivity_Question", ofType: "json")
+        
+        let data = NSData(contentsOfFile: filePath!)
+        
+        
+        do {
+            let dataDict = try JSONSerialization.jsonObject(with: data! as Data, options: []) as? Dictionary<String,Any>
+            
+            if  Utilities.isValidObject(someObject: dataDict as AnyObject?) && (dataDict?.count)! > 0 {
+                
+                
+                let task:ORKTask?
+                let taskViewController:ORKTaskViewController?
+                
+                if Utilities.isValidObject(someObject: dataDict?["Result"] as? Dictionary<String, Any> as AnyObject?){
+                    
+                    
+                    // activityBuilder?.initActivityWithDict(dict: dataDict?["Result"] as! Dictionary<String, Any>)
+                    
+                    
+                    
+                    task = activityBuilder?.createTask()
+                    
+                    
+                    // consentbuilder?.initWithMetaData(metaDataDict:dataDict?["Result"] as! Dictionary<String, Any> )
+                    // task = consentbuilder?.createConsentTask()
+                    
+                    taskViewController = ORKTaskViewController(task:task, taskRun: nil)
+                    
+                    // consentbuilder?.consentResult =   ConsentResult()
+                    // consentbuilder?.consentResult?.consentDocument =  consentbuilder?.consentDocument
+                    
+                    taskViewController?.delegate = self
+                    taskViewController?.outputDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                    present(taskViewController!, animated: true, completion: nil)
+                }
+            }
+            
+            // use anyObj here
+        } catch {
+            print("json error: \(error.localizedDescription)")
+        }
+    }
 }
--- FDA/FDA/Controllers/StudyUI/StudyOverviewUI/StudyOverviewPageViewControllerFirst.swift
+++ FDA/FDA/Controllers/StudyUI/StudyOverviewUI/StudyOverviewPageViewControllerFirst.swift
@@ -58,9 +58,7 @@ class StudyOverviewViewControllerFirst : UIViewController{
         
          labelTitle?.text = overviewSectionDetail.title
         
-        self.labelDescription?.text = overviewSectionDetail.text!
-         WCPServices().getEligibilityConsentMetadata(studyId:(Study.currentStudy?.studyId)! , delegate: self)
-        
+       
         let attrStr = try! NSAttributedString(
             data: (overviewSectionDetail.text?.data(using: String.Encoding.unicode, allowLossyConversion: true)!)!,
             options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
@@ -111,53 +109,8 @@ class StudyOverviewViewControllerFirst : UIViewController{
             let leftController = slideMenuController()?.leftViewController as! LeftMenuViewController
             leftController.changeViewController(.profile_signin)
         }
-        else{
-           self.createEligibilityConsentTask()
-        }
-    }
-    
-    func createEligibilityConsentTask()   {
-        
-        var eligibilitySteps =  EligibilityBuilder.currentEligibility?.getEligibilitySteps()
-       
-        let taskViewController:ORKTaskViewController?
-        
-        
-        
-        
-        let filePath  = Bundle.main.path(forResource: "Consent", ofType: "json")
-         let data = NSData(contentsOfFile: filePath!)
-        do {
-            let dataDict = try JSONSerialization.jsonObject(with: data! as Data, options: []) as? Dictionary<String,Any>
-            
-            let consent = dataDict?["Result"]as! Dictionary<String, Any>
-            ConsentBuilder.currentConsent = ConsentBuilder()
-            ConsentBuilder.currentConsent?.initWithMetaData(metaDataDict: consent)
-            
-        
-        }catch{
-            
-        }
-        
-        let consentTask:ORKOrderedTask? =  ConsentBuilder.currentConsent?.createConsentTask() as! ORKOrderedTask?
-        
-        for stepDict in (consentTask?.steps)!{
-           eligibilitySteps?.append(stepDict)
-        }
-       
-        
-        
-        let orkOrderedTask:ORKTask? = ORKOrderedTask(identifier: "Eligibility_ConsentTask", steps: eligibilitySteps)
-        
-        taskViewController = ORKTaskViewController(task:orkOrderedTask, taskRun: nil)
-        taskViewController?.delegate = self
-        taskViewController?.outputDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        present(taskViewController!, animated: true, completion: nil)
-        
         
     }
-
-    
     
     func moviePlayBackDidFinish(notification: NSNotification) {
         //  println("moviePlayBackDidFinish:")
@@ -207,159 +160,8 @@ extension StudyOverviewViewControllerFirst:NMWebServiceDelegate {
     }
 }
 
-extension StudyOverviewViewControllerFirst:ORKTaskViewControllerDelegate{
-    //MARK:ORKTaskViewController Delegate
+    
+
     
     
-    func taskViewControllerSupportsSaveAndRestore(_ taskViewController: ORKTaskViewController) -> Bool {
-        return true
-    }
-    
-    public func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
-        
-        
-        var taskResult:Any?
-        
-        switch reason {
-            
-        case ORKTaskViewControllerFinishReason.completed:
-            print("completed")
-            taskResult = taskViewController.result
-        case ORKTaskViewControllerFinishReason.failed:
-            print("failed")
-            taskResult = taskViewController.result
-        case ORKTaskViewControllerFinishReason.discarded:
-            print("discarded")
-            taskResult = taskViewController.result
-        case ORKTaskViewControllerFinishReason.saved:
-            print("saved")
-            taskResult = taskViewController.restorationData
-            
-            if taskViewController.task?.identifier == "ConsentTask"{
-                
-            }
-            else{
-                activityBuilder?.activity?.restortionData = taskViewController.restorationData
-            }
-        }
-        
-        if  taskViewController.task?.identifier == "ConsentTask"{
-            consentbuilder?.consentResult?.initWithORKTaskResult(taskResult:taskViewController.result )
-        }
-        else{
-            activityBuilder?.actvityResult?.initWithORKTaskResult(taskResult: taskViewController.result)
-        }
-        
-        
-        taskViewController.dismiss(animated: true, completion: nil)
-        
-    }
-    
-    func taskViewController(_ taskViewController: ORKTaskViewController, stepViewControllerWillAppear stepViewController: ORKStepViewController) {
-        
-        if (taskViewController.result.results?.count)! > 1{
-            
-            
-            if activityBuilder?.actvityResult?.result?.count == taskViewController.result.results?.count{
-                activityBuilder?.actvityResult?.result?.removeLast()
-            }
-            else{
-                
-                if (activityBuilder?.actvityResult?.result?.count)! < (taskViewController.result.results?.count)!{
-                    
-                    let orkStepResult:ORKStepResult? = taskViewController.result.results?[(taskViewController.result.results?.count)! - 2] as! ORKStepResult?
-                    let activityStepResult:ActivityStepResult? = ActivityStepResult()
-                    
-                    activityStepResult?.initWithORKStepResult(stepResult: orkStepResult! as ORKStepResult , activityType:(activityBuilder?.actvityResult?.activity?.type)!)
-                    activityBuilder?.actvityResult?.result?.append(activityStepResult!)
-                    
-                }
-            }
-        }
-    }
-    
-    //MARK:StepViewController Delegate
-    public func stepViewController(_ stepViewController: ORKStepViewController, didFinishWith direction: ORKStepViewControllerNavigationDirection){
-        
-    }
-    
-    public func stepViewControllerResultDidChange(_ stepViewController: ORKStepViewController){
-        
-    }
-    public func stepViewControllerDidFail(_ stepViewController: ORKStepViewController, withError error: Error?){
-        
-    }
-    func taskViewController(_ taskViewController: ORKTaskViewController, viewControllerFor step: ORKStep) -> ORKStepViewController? {
-        
-        if step.identifier == "EligibilityTokenStep" {
-            
-             let gatewayStoryboard = UIStoryboard(name: "FetalKickCounter", bundle: nil)
-            
-            let ttController = gatewayStoryboard.instantiateViewController(withIdentifier: "EligibilityStepViewController") as! EligibilityStepViewController
-            ttController.step = step
-            
-            
-            return ttController
-        } else {
-            return nil
-        }
-    }
-    
-    
-    func buildTask()  {
-        
-        // let filePath  = Bundle.main.path(forResource: "LatestActive_Taskdocument", ofType: "json")
-        
-        //let filePath  = Bundle.main.path(forResource: "ActiveTask", ofType: "json")
-        
-        let filePath  = Bundle.main.path(forResource: "Consent", ofType: "json")
-        
-        //let filePath  = Bundle.main.path(forResource: "Acivity_Question", ofType: "json")
-        
-        let data = NSData(contentsOfFile: filePath!)
-        
-        
-        do {
-            let dataDict = try JSONSerialization.jsonObject(with: data! as Data, options: []) as? Dictionary<String,Any>
-            
-            if  Utilities.isValidObject(someObject: dataDict as AnyObject?) && (dataDict?.count)! > 0 {
-                
-                
-                let task:ORKTask?
-                let taskViewController:ORKTaskViewController?
-                
-                if Utilities.isValidObject(someObject: dataDict?["Result"] as? Dictionary<String, Any> as AnyObject?){
-                    
-                    
-                   // activityBuilder?.initActivityWithDict(dict: dataDict?["Result"] as! Dictionary<String, Any>)
-                    
-                    
-                    
-                    task = activityBuilder?.createTask()
-                    
-                    
-                    // consentbuilder?.initWithMetaData(metaDataDict:dataDict?["Result"] as! Dictionary<String, Any> )
-                    // task = consentbuilder?.createConsentTask()
-                    
-                    taskViewController = ORKTaskViewController(task:task, taskRun: nil)
-                    
-                    // consentbuilder?.consentResult =   ConsentResult()
-                    // consentbuilder?.consentResult?.consentDocument =  consentbuilder?.consentDocument
-                    
-                    taskViewController?.delegate = self
-                    taskViewController?.outputDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-                    present(taskViewController!, animated: true, completion: nil)
-                }
-            }
-            
-            // use anyObj here
-        } catch {
-            print("json error: \(error.localizedDescription)")
-        }
-        
-        
-        
-    }
-    
-    
-}
+

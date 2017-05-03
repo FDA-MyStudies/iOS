2c46e20eadd17835783d55aaa5f88f60810ff660
Arun Kumar <arunkumar@AppleMac.local>
2017-05-03 21:47:06 +0530

    Completion step removed from activityresult api

--- FDA/FDA/ActivityResultModel/ActivityResult.swift
+++ FDA/FDA/ActivityResultModel/ActivityResult.swift
@@ -11,6 +11,8 @@ import Foundation
 import ResearchKit
 
 let kActivityResult = "result"
+let kActivityResults = "results"
+
 
 class ActivityResult {
     
@@ -45,8 +47,11 @@ class ActivityResult {
                 
                 activityStepResult?.initWithORKStepResult(stepResult: stepResult as! ORKStepResult , activityType:(self.activity?.type)!)
                 
+                if stepResult.identifier != "CompletionStep"{
+                     self.result?.append(activityStepResult!)
+                }
                 
-                self.result?.append(activityStepResult!)
+               
                 
                 print("###: \n  \(activityStepResult?.getActivityStepResultDict())")
                 

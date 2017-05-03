450ea91aa8ddcb20b6f2686947d63997a73c9958
Ravishankar <ravishankar@mac-mini-pro.local>
2017-05-03 13:44:39 +0530

    Force Update UI Changes and outlets connected

--- FDA/FDA/ForceUpdate/AppUpdateBlocker.swift
+++ FDA/FDA/ForceUpdate/AppUpdateBlocker.swift
@@ -14,11 +14,13 @@ class AppUpdateBlocker: UIView {
     @IBOutlet var labelMessage:UILabel!
     @IBOutlet var labelVersionNumber:UILabel!
     
-    
     required init?(coder aDecoder: NSCoder) {
         
         super.init(coder: aDecoder)
         //fatalError("init(coder:) has not been implemented")
+        
+        //Used to set border color for bottom view
+        buttonUpgrade?.layer.borderColor = UIColor.white.cgColor
     }
     class func instanceFromNib(frame:CGRect,detail:Dictionary<String,Any>) -> AppUpdateBlocker {
         

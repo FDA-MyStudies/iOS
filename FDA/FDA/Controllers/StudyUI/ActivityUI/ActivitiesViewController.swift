760710041f1b4d0012e2b4786a62eb5cb7aff18f
Arun Kumar <arunkumar@AppleMac.local>
2017-05-06 16:40:50 +0530

    Commented the default hardcoded steps

--- FDA/FDA/Controllers/StudyUI/ActivityUI/ActivitiesViewController.swift
+++ FDA/FDA/Controllers/StudyUI/ActivityUI/ActivitiesViewController.swift
@@ -102,7 +102,7 @@ class ActivitiesViewController : UIViewController{
     
     func createActivity(){
         
-    
+    /*
         let filePath  = Bundle.main.path(forResource: "Labkey_Activity", ofType: "json")
         
         //let filePath  = Bundle.main.path(forResource: "FetalKickTest", ofType: "json")
@@ -118,7 +118,7 @@ class ActivitiesViewController : UIViewController{
             print("\(error)")
         }
  
- 
+ */
         
         if Utilities.isValidObject(someObject: Study.currentActivity?.steps as AnyObject?){
             

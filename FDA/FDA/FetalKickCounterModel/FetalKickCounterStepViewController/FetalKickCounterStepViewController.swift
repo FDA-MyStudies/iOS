fdf51235c8a7cde279bc8983c26fe736bdfe62a5
Arun Kumar <arunkumar@AppleMac.local>
2017-05-12 20:09:10 +0530

    Server updated and key added

--- FDA/FDA/FetalKickCounterModel/FetalKickCounterStepViewController/FetalKickCounterStepViewController.swift
+++ FDA/FDA/FetalKickCounterModel/FetalKickCounterStepViewController/FetalKickCounterStepViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import ResearchKit
 import IQKeyboardManagerSwift
+import IQKeyboardManagerSwift
 
 let kFetalKickCounterStepDefaultIdentifier = "defaultIdentifier"
 
--- FDA/FDA/NetworkModule/WCPConfiguration.swift
+++ FDA/FDA/NetworkModule/WCPConfiguration.swift
@@ -72,8 +72,8 @@ struct WCPServerURLConstants {
     
     
     //local
-   // static let ProductionURL = "http://192.168.0.32:8080/StudyMetaData/"
-   // static let DevelopmentURL = "http://192.168.0.32:8080/StudyMetaData/"
+    //static let ProductionURL = "http://192.168.0.32:8080/StudyMetaData/"
+    //static let DevelopmentURL = "http://192.168.0.32:8080/StudyMetaData/"
     
     
 }

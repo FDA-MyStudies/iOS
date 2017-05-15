34343fa9d9b0b6dec0d82febde8e85fceaa89ea8
Surender Rathore <surenders@boston-technology.com>
2017-05-16 02:11:41 +0530

    Handled assets for stat type Handled data calculation for different time ranges

--- FDA/FDA/Controllers/StudyUI/StudyDashboardUI/StudyDashboardStatisticsCollectionViewCell.swift
+++ FDA/FDA/Controllers/StudyUI/StudyDashboardUI/StudyDashboardStatisticsCollectionViewCell.swift
@@ -8,13 +8,105 @@
 
 import UIKit
 
+enum StatisticsType:String{
+   case  Activity
+   case  Sleep
+   case  Weight
+   case  Nutrition
+   case  HeartRate
+   case  BloodGlucose
+   case  ActiveTask
+   case  BabyKicks
+   case  Mood
+   case  Other
+    
+    var description:String{
+        switch self {
+        case  .Activity:
+            return "Activity"
+        case  .Sleep:
+            return "Sleep"
+        case  .Weight:
+            return "Weight"
+        case  .Nutrition:
+            return "Nutrition"
+        case  .HeartRate:
+            return "Heart Rate"
+        case  .BloodGlucose:
+            return "Blood Glucose"
+        case  .ActiveTask:
+            return "Active Task"
+        case  .BabyKicks:
+            return "Baby Kicks"
+        case  .Mood:
+            return "Mood"
+        case  .Other:
+            return "Other"
+        }
+    }
+}
+
+
 class StudyDashboardStatisticsCollectionViewCell: UICollectionViewCell {
     
     //Statistics cell
     @IBOutlet var statisticsImage : UIImageView?
     @IBOutlet var labelStatisticsText : UILabel?
     @IBOutlet var labelStatisticsCount : UILabel?
+    var stats:DashboardStatistics!
     
+    
+    func displayStatisics(data : DashboardStatistics,startDate:Date,endDate:Date?,tab:SelectedTab){
+        
+         stats = data
+         labelStatisticsText?.text = data.displayName
+        
+        self.displayStateTypeImage()
+        
+        switch tab {
+        case .Day:
+            self.handleForDay(date: startDate)
+        case .Week:
+            self.handleForWeek(startDate: startDate, endDate: endDate!)
+        case .Month:
+            self.handleForMonth(date: startDate)
+        }
+    }
+    
+    func displayStateTypeImage(){
+        
+        if stats.statType == nil {
+            statisticsImage?.image = UIImage(named: "activity")
+        }
+        else {
+            let type = StatisticsType(rawValue:stats.statType!)!
+            
+            switch type {
+            case  .Activity:
+                statisticsImage?.image = UIImage(named: "stat_icn_active_task")
+            case  .Sleep:
+                statisticsImage?.image = UIImage(named: "stat_icn_sleep")
+            case  .Weight:
+                statisticsImage?.image = UIImage(named: "stat_icn_weight")
+            case  .Nutrition:
+                statisticsImage?.image = UIImage(named: "stat_icn_nutrition")
+            case  .HeartRate:
+                statisticsImage?.image = UIImage(named: "stat_icn_heart_rate")
+            case  .BloodGlucose:
+                statisticsImage?.image = UIImage(named: "stat_icn_glucose")
+            case  .ActiveTask:
+                statisticsImage?.image = UIImage(named: "stat_icn_active_task")
+            case  .BabyKicks:
+                statisticsImage?.image = UIImage(named: "stat_icn_baby_kicks")
+            case  .Mood:
+                statisticsImage?.image = UIImage(named: "stat_icn_mood")
+            case  .Other:
+                statisticsImage?.image = UIImage(named: "stat_icn_other")
+            }
+        }
+        
+       
+    }
     
     /**
      
@@ -62,4 +154,74 @@ class StudyDashboardStatisticsCollectionViewCell: UICollectionViewCell {
 //        }
 
     }
+    
+    
+    //MARK:- Data Handler
+    func handleForDay(date:Date){
+        
+        let dataList:Array<DBStatisticsData> = stats.statList.filter({$0.startDate! >= date.startOfDay && $0.startDate! <= date.endOfDay!})
+        
+        let array = dataList.map{$0.data}
+        
+        if array.count == 0{
+            labelStatisticsCount?.text = "NA"
+        }
+        else {
+            self.calculate(array: array)
+        }
+    }
+    func handleForWeek(startDate:Date,endDate:Date){
+        
+        let dataList:Array<DBStatisticsData> = stats.statList.filter({$0.startDate! >= startDate && $0.startDate! <= endDate})
+        
+        let array = dataList.map{$0.data}
+        
+        if array.count == 0{
+            labelStatisticsCount?.text = "NA"
+        }
+        else {
+            self.calculate(array: array)
+        }
+        
+    }
+    func handleForMonth(date:Date){
+        
+        let dataList:Array<DBStatisticsData> = stats.statList.filter({$0.startDate! >= date.startOfMonth() && $0.startDate! <= date.endOfMonth()})
+        
+        let array = dataList.map{$0.data}
+        
+        if array.count == 0{
+            labelStatisticsCount?.text = "NA"
+        }
+        else {
+           self.calculate(array: array)
+        }
+        
+    }
+    
+    func calculate(array:Array<Float>){
+        
+        
+       // let array = data.statList.map{$0.data}
+        let data = self.stats!
+        
+        if  data.calculation! == StatisticsFormula.Maximum.rawValue {
+            let max = array.max()
+            labelStatisticsCount?.text = String(describing: max)
+        }
+        if  data.calculation! == StatisticsFormula.Minimum.rawValue {
+            let min = array.min()
+            labelStatisticsCount?.text = String(describing: min)
+        }
+        if  data.calculation! == StatisticsFormula.Average.rawValue {
+            let sumArray = array.reduce(0, +)
+            let avgArrayValue = sumArray / Float(array.count)
+            labelStatisticsCount?.text = String(describing: avgArrayValue)
+        }
+        if  data.calculation! == StatisticsFormula.Summation.rawValue {
+            let sumArray = array.reduce(0, +)
+            labelStatisticsCount?.text = String(describing: sumArray)
+        }
+    }
+
 }
--- FDA/FDA/Controllers/StudyUI/StudyDashboardUI/StudyDashboardStatisticsTableViewCell.swift
+++ FDA/FDA/Controllers/StudyUI/StudyDashboardUI/StudyDashboardStatisticsTableViewCell.swift
@@ -8,15 +8,33 @@
 
 import UIKit
 
+
+
+enum SelectedTab:String{
+    case Day
+    case Week
+    case Month
+}
 class StudyDashboardStatisticsTableViewCell: UITableViewCell {
 
+    
+   
+    
     //Fifth cell Outlets
     @IBOutlet var statisticsCollectionView: UICollectionView?
     @IBOutlet var buttonDay : UIButton?
     @IBOutlet var buttonWeek : UIButton?
     @IBOutlet var buttonMonth : UIButton?
+    @IBOutlet var buttonForward : UIButton?
+    @IBOutlet var buttonBackward : UIButton?
+    @IBOutlet var labelDateValue : UILabel?
+    @IBOutlet var labelNoData : UILabel?
     
     var statisticsArrayData : NSMutableArray?
+    var selectedTab:SelectedTab = .Day
+    var todaysDate = Date()
+    var startDateOfWeek:Date?
+    var endDateOfWeek:Date?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,6 +46,18 @@ class StudyDashboardStatisticsTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
         //Configure the view for the selected state
         
+    }
+    
+    func displayData(){
+        
+        let stringDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: todaysDate)
+        self.labelDateValue?.text = stringDate
+        self.buttonForward?.isEnabled = false
+        
+        if StudyDashboard.instance.statistics.count == 0  {
+            labelNoData?.isHidden = false
+            self.statisticsCollectionView?.isHidden = true
+        }
     }
     
 //MARK:- Button action
@@ -55,6 +85,13 @@ class StudyDashboardStatisticsTableViewCell: UITableViewCell {
             buttonWeek?.backgroundColor = UIColor.white
             buttonMonth?.backgroundColor = UIColor.white
             
+            self.selectedTab = .Day
+             todaysDate = Date()
+            
+            let stringDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: Date())
+            self.labelDateValue?.text = stringDate
+            self.buttonForward?.isEnabled = false
+            
         }else if sender.tag == 12{
             //Week clicked
             buttonWeek?.setTitle(kWeekSpaces, for: UIControlState.normal)
@@ -69,7 +106,24 @@ class StudyDashboardStatisticsTableViewCell: UITableViewCell {
             buttonDay?.backgroundColor = UIColor.white
             buttonMonth?.backgroundColor = UIColor.white
             
+            self.selectedTab = .Week
+            todaysDate = Date()
+            
+            
+            
+            startDateOfWeek = todaysDate.startOfWeek
+            endDateOfWeek = todaysDate.endOfWeek
+            //current year
+            let stringStartDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: startDateOfWeek!)
+            let stringEndDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: endDateOfWeek!)
+            labelDateValue?.text = stringStartDate + " - " + stringEndDate
+            self.buttonForward?.isEnabled = false
+
+            
         }else if sender.tag == 13{
+            
+          
+            
             //Months clicked
             buttonMonth?.setTitle(kMonthSpaces, for: UIControlState.normal)
             buttonDay?.setTitle(kDay, for: UIControlState.normal)
@@ -82,8 +136,144 @@ class StudyDashboardStatisticsTableViewCell: UITableViewCell {
             buttonMonth?.backgroundColor = kDarkBlueColor
             buttonDay?.backgroundColor = UIColor.white
             buttonWeek?.backgroundColor = UIColor.white
+            
+            self.selectedTab = .Month
+            todaysDate = Date()
+            
+            let stringDate = StudyDashboardStatisticsTableViewCell.monthFormatter.string(from: todaysDate)
+            self.labelDateValue?.text = stringDate
+            self.buttonForward?.isEnabled = false
+        }
+        
+        self.statisticsCollectionView?.reloadData()
+    }
+    
+    @IBAction func buttonForwardClicked(_ sender:UIButton){
+        
+        let calendar = Calendar.current
+        
+        switch self.selectedTab {
+        case .Day:
+            
+            self.buttonForward?.isEnabled = true
+            self.buttonBackward?.isEnabled = true
+            todaysDate = calendar.date(byAdding: .day, value: 1, to: todaysDate)!
+            let stringDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: todaysDate)
+            labelDateValue?.text = stringDate
+            
+            
+            
+            let result = todaysDate.compare(Date())
+            if result == .orderedSame || result == .orderedDescending{
+                self.buttonForward?.isEnabled = false
+            }
+            
+        case .Week:
+            
+            self.buttonForward?.isEnabled = true
+            self.buttonBackward?.isEnabled = true
+            
+            startDateOfWeek = calendar.date(byAdding: .day, value: 7, to: startDateOfWeek!)
+            endDateOfWeek = calendar.date(byAdding: .day, value: 7, to: endDateOfWeek!)
+            let stringStartDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: startDateOfWeek!)
+            let stringEndDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: endDateOfWeek!)
+            labelDateValue?.text = stringStartDate + " - " + stringEndDate
+            
+            let result = todaysDate.compare(Date())
+            if result == .orderedSame || result == .orderedDescending{
+                self.buttonForward?.isEnabled = false
+            }
+
+            
+        case .Month:
+            
+            self.buttonForward?.isEnabled = true
+            self.buttonBackward?.isEnabled = true
+            todaysDate = calendar.date(byAdding: .month, value: 1, to: todaysDate)!
+            let stringDate = StudyDashboardStatisticsTableViewCell.monthFormatter.string(from: todaysDate)
+            labelDateValue?.text = stringDate
+            
+            
+            
+            let result = todaysDate.compare(Date())
+            if result == .orderedSame || result == .orderedDescending{
+                self.buttonForward?.isEnabled = false
+            }
+        
         }
     }
+    @IBAction func buttonBackwardClicked(_ sender:UIButton){
+        
+        let calendar = Calendar.current
+        
+        switch self.selectedTab {
+        case .Day:
+            
+            self.buttonForward?.isEnabled = true
+            self.buttonBackward?.isEnabled = true
+            todaysDate = calendar.date(byAdding: .day, value: -1, to: todaysDate)!
+            let stringDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: todaysDate)
+            labelDateValue?.text = stringDate
+            
+            
+            
+//            let result = todaysDate.compare(Date())
+//            if result == .orderedSame || result == .orderedDescending{
+//                self.buttonForward?.isEnabled = false
+//            }
+            
+        case .Week:
+            
+            self.buttonForward?.isEnabled = true
+            self.buttonBackward?.isEnabled = true
+            
+            startDateOfWeek = calendar.date(byAdding: .day, value: -7, to: startDateOfWeek!)
+            endDateOfWeek = calendar.date(byAdding: .day, value: -7, to: endDateOfWeek!)
+            let stringStartDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: startDateOfWeek!)
+            let stringEndDate = StudyDashboardStatisticsTableViewCell.formatter.string(from: endDateOfWeek!)
+            labelDateValue?.text = stringStartDate + " - " + stringEndDate
+            
+        case .Month:
+            
+            self.buttonForward?.isEnabled = true
+            self.buttonBackward?.isEnabled = true
+            todaysDate = calendar.date(byAdding: .month, value: -1, to: todaysDate)!
+            let stringDate = StudyDashboardStatisticsTableViewCell.monthFormatter.string(from: todaysDate)
+            labelDateValue?.text = stringDate
+            
+            
+            
+//            let result = todaysDate.compare(Date())
+//            if result == .orderedSame || result == .orderedDescending{
+//                self.buttonForward?.isEnabled = false
+//            }
+            
+        }
+        
+        self.statisticsCollectionView?.reloadData()
+    }
+    
+    
+        
+    //MARK: - FORMATERS
+    private static let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMM YYYY"
+        formatter.timeZone = TimeZone.init(abbreviation:"GMT")
+        return formatter
+    }()
+    private static let yearFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY"
+        formatter.timeZone = TimeZone.init(abbreviation:"GMT")
+        return formatter
+    }()
+    private static let monthFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM YYYY"
+        formatter.timeZone = TimeZone.init(abbreviation:"GMT")
+        return formatter
+    }()
 }
 
 //MARK:- Collection delegates
@@ -98,7 +288,13 @@ extension StudyDashboardStatisticsTableViewCell: UICollectionViewDelegate, UICol
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kStatisticsCollectionViewCell, for: indexPath) as! StudyDashboardStatisticsCollectionViewCell
         let stats = StudyDashboard.instance.statistics[indexPath.row]
-        cell.displayStatisics(data: stats)
+        //cell.displayStatisics(data: stats)
+        if selectedTab == .Week {
+           cell.displayStatisics(data: stats, startDate: startDateOfWeek!, endDate: endDateOfWeek, tab: selectedTab)
+        }
+        else {
+            cell.displayStatisics(data: stats, startDate: todaysDate, endDate: nil, tab: selectedTab)
+        }
         
         return cell
     }

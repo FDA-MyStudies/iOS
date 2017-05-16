e6b8bfda4b90c72f02212e8d1674b3fd1417ca6b
Arun Kumar <arunkumar@AppleMac.local>
2017-05-16 20:10:58 +0530

    Jailbroke functionality implemented

---
+++ FDA/FDA/JailbrokeBlocker/JailbrokeBlocker.swift
@@ -0,0 +1,38 @@
+//
+//  AppUpdateBlocker.swift
+//  FDA
+//
+//  Created by Surender Rathore on 5/2/17.
+//  Copyright © 2017 BTC. All rights reserved.
+//
+
+import UIKit
+
+class AppUpdateBlocker: UIView {
+
+    @IBOutlet var buttonUpgrade:UIButton!
+    @IBOutlet var labelMessage:UILabel!
+    @IBOutlet var labelVersionNumber:UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        //fatalError("init(coder:) has not been implemented")
+        
+        //Used to set border color for bottom view
+        buttonUpgrade?.layer.borderColor = UIColor.white.cgColor
+    }
+    class func instanceFromNib(frame:CGRect,detail:Dictionary<String,Any>) -> AppUpdateBlocker {
+        
+        let view = UINib(nibName: "AppUpdateBlocker", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! AppUpdateBlocker
+        view.frame = frame
+        view.layoutIfNeeded()
+        return view
+        
+    }
+
+    @IBAction func buttonUpgradeAction(){
+        UIApplication.shared.openURL(URL(string:"https://itunes.apple.com/us/app/hey-there.../id1032962936?ls=1&mt=8")!)
+    }
+
+}

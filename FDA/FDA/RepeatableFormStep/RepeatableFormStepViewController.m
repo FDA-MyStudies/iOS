514ee9198039c8d930f79956b8259d9729b79fd7
Arun Kumar <arunkumar@AppleMac.local>
2017-06-01 21:20:57 +0530

    Repeatable form step scrolling issue fixed

--- FDA/FDA/RepeatableFormStep/RepeatableFormStepViewController.m
+++ FDA/FDA/RepeatableFormStep/RepeatableFormStepViewController.m
@@ -97,6 +97,9 @@
     }
 }
 
+- (void)tableView:(UITableView* )tableView willDisplayCell:(UITableViewCell* )cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    _originalTableView = tableView;
+}
 
 
 
@@ -129,10 +132,20 @@
     [super stepDidChange];
     
    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if (self.originalTableView.contentSize.height > self.originalTableView.frame.size.height)
+        {
+            CGPoint offset = CGPointMake(0, self.originalTableView.contentSize.height - self.originalTableView.frame.size.height);
+            [self.originalTableView setContentOffset:offset animated:YES];
+        }
+        
+    });
+
     
     
-    NSIndexPath *ip = [NSIndexPath indexPathForRow:mItems.count - 1 inSection:0];
-    [_originalTableView scrollToRowAtIndexPath:ip atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    
+    //NSIndexPath *ip = [NSIndexPath indexPathForRow:mItems.count - 1 inSection:0];
+   // [_originalTableView scrollToRowAtIndexPath:ip atScrollPosition:UITableViewScrollPositionBottom animated:YES];
 }
 
 

d01dff10d0e4b68b0321455a718e2c0795eaf64c
Surender Rathore <surenders@boston-technology.com>
2017-06-02 18:48:21 +0530

    Fixed crash issue if activity is nil

--- FDA/FDA/SyncUpdate/SyncUpdate.swift
+++ FDA/FDA/SyncUpdate/SyncUpdate.swift
@@ -56,51 +56,72 @@ class SyncUpdate{
             let run = dbRuns.last
             self.selectedRun = run
             
-            let activity = realm.object(ofType: DBActivity.self, forPrimaryKey: run?.activityId)
+            let activity = realm.object(ofType: DBActivity.self, forPrimaryKey: run?.activityId!)
             
-            let currentUser = User.currentUser
-            if let userStudyStatus = currentUser.participatedStudies.filter({$0.studyId == run?.studyId}).first {
+            if activity == nil{
+                self.findNextRunToSync()
+            }
+            else if activity != nil && activity?.shortName == nil{
+                self.findNextRunToSync()
+            }
+            else {
                 
-                
-                
-                
-                let studyId =  run?.studyId
-                let activiyId =  run?.activityId
-                let activityName =  activity?.shortName
-                let activityVersion = activity?.version!
-                let currentRunId = run?.runId
-                
-                
-                
-                let info =  [kStudyId:studyId! ,
-                             kActivityId:activiyId! ,
-                             kActivityName:activityName! ,
-                             "version" :activityVersion! ,
-                             kActivityRunId:"\(currentRunId!)"
-                    ] as [String : String]
-                
-                let ActivityType = activity?.type
-                
-                let participationid = userStudyStatus.participantId
-                
-                var data:Dictionary<String, Any> = [:]
-                do {
-                    let responseData = try JSONSerialization.jsonObject(with: (run?.responseData)!, options: []) as! [String:Any]
-                    data = (responseData["data"] as! Dictionary<String, Any>?)!
-                }
-                catch{
+                let currentUser = User.currentUser
+                if let userStudyStatus = currentUser.participatedStudies.filter({$0.studyId == run?.studyId}).first {
+                    
+                    
+                    
+                    
+                    let studyId =  run?.studyId
+                    let activiyId =  run?.activityId
+                    let activityName =  activity?.shortName
+                    let activityVersion = activity?.version!
+                    let currentRunId = run?.runId
+                    
+                    
+                    
+                    let info =  [kStudyId:studyId! ,
+                                 kActivityId:activiyId! ,
+                                 kActivityName:activityName! ,
+                                 "version" :activityVersion! ,
+                                 kActivityRunId:"\(currentRunId!)"
+                        ] as [String : String]
+                    
+                    let ActivityType = activity?.type
+                    
+                    let participationid = userStudyStatus.participantId
+                    
+                    var data:Dictionary<String, Any> = [:]
+                    do {
+                        let responseData = try JSONSerialization.jsonObject(with: (run?.responseData)!, options: []) as! [String:Any]
+                        data = (responseData["data"] as! Dictionary<String, Any>?)!
+                    }
+                    catch{
+                        
+                    }
+                    
+                    LabKeyServices().processResponse(metaData: info, activityType: ActivityType!, responseData: data, participantId: participationid!, delegate: self)
                     
                 }
-                
-                LabKeyServices().processResponse(metaData: info, activityType: ActivityType!, responseData: data, participantId: participationid!, delegate: self)
-                
             }
-            
-            
+         
             
         }
         else{
             self.selectedRun = nil
+        }
+    }
+    
+    func findNextRunToSync(){
+        if selectedRun != nil {
+            
+            let realm = try! Realm()
+            try! realm.write {
+                self.selectedRun?.toBeSynced = false
+                self.selectedRun?.responseData = nil
+            }
+            
+            self.updateRunInformationToServer()
         }
     }
     
@@ -119,16 +140,7 @@ extension SyncUpdate:NMWebServiceDelegate {
     func finishedRequest(_ manager: NetworkManager, requestName: NSString, response: AnyObject?) {
         Logger.sharedInstance.info("requestname : \(requestName) : \(response)")
         
-        if selectedRun != nil {
-            
-            let realm = try! Realm()
-            try! realm.write {
-                self.selectedRun?.toBeSynced = false
-                self.selectedRun?.responseData = nil
-            }
-            
-            self.updateRunInformationToServer()
-        }
+       self.findNextRunToSync()
         
        
     }

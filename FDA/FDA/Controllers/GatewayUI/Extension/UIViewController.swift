ada306f7ef2da86714a0b528f567c8c7e7b743ec
Surender Rathore <surenders@boston-technology.com>
2017-06-04 22:28:47 +0530

    Added extension to get top view controller

--- FDA/FDA/Controllers/GatewayUI/Extension/UIViewController.swift
+++ FDA/FDA/Controllers/GatewayUI/Extension/UIViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 extension UIViewController {
     
+    
+    func topMostViewController() -> UIViewController {
+        
+            if self.presentedViewController == nil {
+                return self
+            }
+            if let navigation = self.presentedViewController as? UINavigationController {
+                return navigation.visibleViewController!.topMostViewController()
+            }
+            if let tab = self.presentedViewController as? UITabBarController {
+                if let selectedTab = tab.selectedViewController {
+                    return selectedTab.topMostViewController()
+                }
+                return tab.topMostViewController()
+            }
+            return self.presentedViewController!.topMostViewController()
+    }
+    
     func setNavigationBarItem() {
         self.addLeftBarButtonWithImage(UIImage(named: "menu_icn")!)
         //self.addRightBarButtonWithImage(UIImage(named: "")!)

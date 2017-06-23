737afe5364819b77cf45e445eecae1f9b9d3c630
Surender Rathore <surenders@boston-technology.com>
2017-06-23 19:04:54 +0530

    Implemented Realm database migration.

--- FDA/FDA/AppDelegate.swift
+++ FDA/FDA/AppDelegate.swift
@@ -10,7 +10,7 @@
     import Fabric
     import Crashlytics
     import UserNotifications
-    
+    import RealmSwift
     import CallKit
     
     let kEncryptionKey = "EncryptionKey"
@@ -197,6 +197,34 @@
             LocalNotification.registerReopenAppNotification()
             
         }
+        //MARK: Realm Migragion
+        func checkForRealmMigration(){
+            
+            // Inside your application(application:didFinishLaunchingWithOptions:)
+            
+            let config = Realm.Configuration(
+                // Set the new schema version. This must be greater than the previously used
+                // version (if you've never set a schema version before, the version is 0).
+                schemaVersion: 1,
+                
+                // Set the block which will be called automatically when opening a Realm with
+                // a schema version lower than the one set above
+                migrationBlock: { migration, oldSchemaVersion in
+                    // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                    if (oldSchemaVersion < 1) {
+                        // Nothing to do!
+                        // Realm will automatically detect new properties and removed properties
+                        // And will update the schema on disk automatically
+                    }
+            })
+            
+            // Tell Realm to use this new configuration object for the default Realm
+            Realm.Configuration.defaultConfiguration = config
+            
+            // Now that we've told Realm how to handle the schema change, opening the file
+            // will automatically perform the migration
+            let realm = try! Realm()
+        }
         
         //MARK: App Delegates
         
@@ -257,7 +285,7 @@
                 
             }
             
-            
+            self.checkForRealmMigration()
             
             //LabKeyServices().getParticipantResponse(activityId: "Q1", participantId: "1a3d0d308df81024f8bfd7f11f7a0168", delegate: self)
             

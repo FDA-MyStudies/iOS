f3bf8812088fd6d5725625caef1df957753d9ab7
Surender Rathore <surenders@boston-technology.com>
2017-07-17 11:05:21 +0530

    Bug #14752 Mobile App > Sign Out > Data lost message

--- FDA/FDA/Controllers/GatewayUI/SlideMenu/LeftMenuViewController.swift
+++ FDA/FDA/Controllers/GatewayUI/SlideMenu/LeftMenuViewController.swift
@@ -20,8 +20,8 @@ let kLeftMenuCellTitleNewUser = "New User?"
 let kLeftMenuCellSubTitleValue = "Sign up"
 let kAlertMessageReachoutText = "This feature will be available in the next sprint."
 
-let kAlertMessageForSignOut = "Are you sure you want to Sign Out ?"
-let kAlertMessageSignOutSync = "Are you sure you want to Sign Out ? Your data will be lost."
+let kAlertMessageForSignOut = "Are you sure you want to sign out?"
+let kAlertMessageSignOutSync = "Are you sure you want to sign out? Incomplete activities and activities completed while offline must be re-started when you next sign in."
 
 let kAlertSignOutLaterTitle = "Sign Out later"
 

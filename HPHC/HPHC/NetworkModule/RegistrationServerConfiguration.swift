13d838e9edcf215f3b067110441437217f6e897f
Surender <surenders@boston-technology.com>
2019-06-14 19:54:08 +0530

    Limit jia branding and build sharing

--- HPHC/HPHC/NetworkModule/RegistrationServerConfiguration.swift
+++ HPHC/HPHC/NetworkModule/RegistrationServerConfiguration.swift
@@ -77,8 +77,8 @@ struct RegistrationServerURLConstants {
     
     
     //Staging server
-    //static let ProductionURL = "https://hpreg-stage.lkcompliant.net/fdahpUserRegWS/"
-    //static let DevelopmentURL = "https://hpreg-stage.lkcompliant.net/fdahpUserRegWS/"
+    static let ProductionURL = "https://hpreg-stage.lkcompliant.net/fdahpUserRegWS/"
+    static let DevelopmentURL = "https://hpreg-stage.lkcompliant.net/fdahpUserRegWS/"
   
     
     // Local - naveen
@@ -86,8 +86,8 @@ struct RegistrationServerURLConstants {
     //static let DevelopmentURL = "http://192.168.0.125:8081/labkey/fdahpUserRegWS/"
     
     //local - btc
-    static let ProductionURL = "http://192.168.0.44:8085/labkey/fdahpUserRegWS/"
-    static let DevelopmentURL = "http://192.168.0.44:8085/labkey/fdahpUserRegWS/"
+    //static let ProductionURL = "http://192.168.0.44:8085/labkey/fdahpUserRegWS/"
+    //static let DevelopmentURL = "http://192.168.0.44:8085/labkey/fdahpUserRegWS/"
     
     
    
